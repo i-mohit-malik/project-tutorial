34f2530010ad82d0fea1fd6f79c12844a60095bc
i-mohit-malik <mohitkumarmalik100@gmail.com>
2026-02-24 18:12:57 +0530

    Repo_pack made and imported to setup

---
+++ setup..py
@@ -0,0 +1,6 @@
+from repo_pack import functions
+
+print(functions.add(10, 5))        # Output: 15
+print(functions.subtract(10, 5))   # Output: 5  
+print(functions.multiply(10, 5))   # Output: 50
+print(functions.divide(10, 5))     # Output: 2.0
