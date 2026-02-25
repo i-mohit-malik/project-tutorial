fdf6613e110b56beb150b5c2498d624e3ef7e327
i-mohit-malik <mohitkumarmalik100@gmail.com>
2026-02-25 10:30:27 +0530

    api2 added

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from repo_pack import functions
-from api_handling import api
+from api_handling import api, api2
 
 print(functions.add(10, 5))        # Output: 15
 print(functions.subtract(10, 5))   # Output: 5  
@@ -7,4 +7,7 @@ print(functions.multiply(10, 5))   # Output: 50
 print(functions.divide(10, 5))     # Output: 2.0
 
 
-api.main()
+api.user()
+
+api2.firm()
+
